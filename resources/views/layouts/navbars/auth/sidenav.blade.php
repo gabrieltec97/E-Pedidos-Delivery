04836db5d541b9fd73ee59d85b7a4cfe277e504d
gabrieltec97 <gabrielphp20@gmail.com>
2024-12-29 12:19:24 -0300

    teste

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -76,7 +76,7 @@
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-utensils text-dark text-sm opacity-10"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Itenss</span>
+                    <span class="nav-link-text ms-1">Itens</span>
                 </a>
                 <div class="collapse" id="submenuProducts">
                     <ul class="navbar-nav ms-4">
