2d755f63723dacbf5cfa962c72f4f10715236faa
Gabriel Pereira <47263072+gabrieltec97@users.noreply.github.com>
2025-01-04 11:48:41 -0300

    commit
    ajustes dinâmicos para chart 2

--- app/Charts/MonthChart.php
+++ app/Charts/MonthChart.php
@@ -15,11 +15,19 @@ class MonthChart
 
     public function build(): \ArielMejiaDev\LarapexCharts\AreaChart
     {
+
+        $month = 4; // Janeiro
+        $year = 2025;
+
+        $days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        $field = range(1, $days);
+
+
         return $this->chart2->areaChart()
             ->setTitle('Sales during 2021.')
             ->setSubtitle('Physical sales vs Digital sales.')
             ->addData('Physical sales', [40, 93, 35, 42, 18, 82])
-            ->addData('Digital sales', [70, 29, 77, 28, 55, 45])
-            ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June']);
+            ->addData('Digital sales', [70, 29, 77, 28, 55, 45,55,54,5465,6,65,65,65,65,65,65,65,65,65,0,54,045,45,0,0])
+            ->setXAxis($field);
     }
 }
