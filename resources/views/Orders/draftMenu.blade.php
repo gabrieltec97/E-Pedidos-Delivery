0857dbd03605e9e71c0a4120840ef80f30569a77
Gabriel Pereira <gabrieldeveloper23@hotmail.com>
2025-03-16 15:03:39 -0300

    Ajustando bug de nome atrás da foto no card de itens (burguers)

--- resources/views/Orders/draftMenu.blade.php
+++ resources/views/Orders/draftMenu.blade.php
@@ -195,13 +195,13 @@
                                         </p>
                                         <input type="hidden" name="productId" value="{{ $burguer->id }}">
                                         <input type="number" class="form-control" name="ammount" style="width: 90px" value="1" hidden="">
-                                        <button type="button" class="btn btn-add no-mobile" data-toggle="modal" data-target=".modalItem{{ $burguer->id }}"><i class="fas fa-shopping-bag"></i></button>
-                                        <input type="number" hidden class="ammount" name="ammount">
-{{--                                        <div class="add-tray">--}}
+                                        <div class="add-tray">
 {{--                                            <span class="btn-less"><i class="fas fa-minus"></i></span>--}}
 {{--                                            <span class="add-number-items">1</span>--}}
 {{--                                            <span class="btn-plus"><i class="fas fa-plus"></i></span>--}}
-{{--                                        </div>--}}
+                                            <button type="button" class="btn btn-add w-100 no-mobile" data-toggle="modal" data-target=".modalItem{{ $burguer->id }}"><i class="fas fa-shopping-bag"></i></button>
+                                            <input type="number" hidden class="ammount" name="ammount">
+                                        </div>
                                     </div>
 
                                     <!-- Modal de cada item-->
