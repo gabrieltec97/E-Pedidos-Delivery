25956a483352ddb13b44024ce6f8ac957945d8d9
Gabriel Pereira <47263072+gabrieltec97@users.noreply.github.com>
2025-03-29 14:12:18 -0300

    inserção de primeiro passo da criação correta de cep.

--- resources/views/Neighbourhoods/new-neighbourhood.blade.php
+++ resources/views/Neighbourhoods/new-neighbourhood.blade.php
@@ -21,19 +21,25 @@
                         <div class="card-body">
                             <p class="text-sm">Cadastre um novo bairro atendido para entregas</p>
                             <div class="row">
-                                <div class="col-md-3">
+                                <div class="col-12 col-lg-3 col-md-3 col-sm-12">
                                     <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">Nome</label>
-                                        <input class="form-control" type="text" placeholder="Nome do bairro" name="name" required>
+                                        <label for="example-text-input" class="form-control-label">Cep</label>
+                                        <input class="form-control" id="txtCEP" type="text" placeholder="Cep de uma rua deste bairro" name="name" required>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-12 col-lg-3 col-md-3 col-sm-12">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Nome</label>
+                                        <input class="form-control" type="text" id="txtBairro" placeholder="Nome do bairro" name="name" required>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-3 col-md-3 col-sm-12">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Taxa de entrega</label>
                                         <input class="form-control" type="number" placeholder="Valor da entrega" name="taxe" required>
                                     </div>
                                 </div>
-                                <div class="col-md-3">
+                                <div class="col-12 col-lg-3 col-md-3 col-sm-12">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Tempo médio de entrega</label>
                                         <select class="form-control" name="time" required>
@@ -48,9 +54,9 @@
                                     </div>
                                 </div>
 
-                                <div class="col-md-3">
+                                <div class="col-12 col-lg-3 col-md-3 col-sm-12">
                                     <div class="form-group">
-                                        <input type="checkbox" name="is_available" id="available" style="margin-top: 45px;" checked>
+                                        <input type="checkbox" name="is_available" id="available" checked>
                                         <label for="available">Atendendo agora</label>
                                     </div>
                                 </div>
@@ -62,4 +68,50 @@
             </form>
         </div>
     </div>
+
+    <script>
+        $(document).ready(function(){
+            //API de busca de cep.
+        function buscarCep(){
+            var cep = $('#txtCEP').val().trim().replace(/\D/g,'');
+    
+            if (cep != ""){
+                var cepValidate = /^[0-9]{8}$/;
+    
+                if (cepValidate.test(cep)){
+                    $.getJSON("http://viacep.com.br/ws/" + cep + "/json/?callback=?", function (data){
+    
+                        if (!("erro" in data)){
+                            $('#txtBairro').val(data.bairro);
+    
+                        }else{
+                            $.toast({
+                                heading: '<b>Cep inválido!</b>',
+                                showHideTransition: 'slide',
+                                bgColor: 'red',
+                                text: 'Não conseguimos encontrar este cep, preencha as informações manualmente.',
+                                hideAfter: 8000,
+                                position: 'top-right',
+                                textColor: 'white',
+                                icon: 'error'
+                            });
+                            $('#txtCEP').val('');
+                        }
+                    })
+                }else{
+                   //Tratar com toast de cep inválido
+                }
+    
+            }else{
+                $('#txtCEP').focus();
+            }
+        }
+    
+        $("#txtCEP").blur(function() {
+            buscarCep();
+        });
+        });
+    </script>
 @endsection
+
+
