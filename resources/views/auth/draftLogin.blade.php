f89421993514eed22066c2981937924998422dcf
Gabriel Pereira <47263072+gabrieltec97@users.noreply.github.com>
2025-04-14 12:55:21 -0300

    inicio de desenvolvimento de recuperação de senha

--- resources/views/auth/draftLogin.blade.php
+++ resources/views/auth/draftLogin.blade.php
@@ -22,50 +22,64 @@
                 <img src="img/img-01.png" alt="IMG">
             </div>
 
-            <form class="login100-form validate-form" action="{{ route('login.perform') }}" method="post">
-                @csrf
-					<span class="login100-form-title">
-						Área de membros
-					</span>
+            <div>
+                <form class="login100-form validate-form" action="{{ route('login.perform') }}" method="post">
+                    @csrf
+                        <span class="login100-form-title">
+                            Área de membros
+                        </span>
+    
+                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                        <input class="input100" type="text" name="email" placeholder="Email">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                            </span>
+                    </div>
+    
+                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                        <input class="input100" type="password" name="password" placeholder="Senha">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                                <i class="fa fa-lock" aria-hidden="true"></i>
+                            </span>
+                    </div>
+    
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" type="submit">
+                            Login
+                        </button>
+                    </div>
+    
+                    <div class="text-center p-t-12">
+                            <span class="txt1">
+                                Esqueceu
+                            </span>
+                        <a class="txt2 pass-recover" href="#">
+                            sua senha?
+                        </a>
+                    </div>
+    
+                    <div class="text-center p-t-136">
+                        <a class="txt2" href="#">
+                            Suporte E-Pedidos
+                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
 
-                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                    <input class="input100" type="text" name="email" placeholder="Email">
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
-						</span>
-                </div>
-
-                <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                    <input class="input100" type="password" name="password" placeholder="Senha">
-                    <span class="focus-input100"></span>
-                    <span class="symbol-input100">
-							<i class="fa fa-lock" aria-hidden="true"></i>
-						</span>
-                </div>
-
-                <div class="container-login100-form-btn">
-                    <button class="login100-form-btn" type="submit">
-                        Login
-                    </button>
-                </div>
-
-                <div class="text-center p-t-12">
-						<span class="txt1">
-							Esqueceu
-						</span>
-                    <a class="txt2" href="#">
-                        sua senha?
-                    </a>
-                </div>
-
-                <div class="text-center p-t-136">
-                    <a class="txt2" href="#">
-                        Suporte E-Pedidos
-                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                    </a>
-                </div>
-            </form>
+            <div class="div-recover">
+                <form action="#" method="post">
+                    @csrf
+                    <span class="login100-form-title text-black font-weight-bold">
+                        Recuperação de senha:
+                    </span>
+                    <label for="email" class="text-black">Digite seu e-mail:</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                    <button class="btn btn-primary mt-4 w-100 font-weight-bold">Enviar código</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
@@ -78,6 +92,7 @@
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/js/select2.min.js') }}"></script>
 <script src="{{ asset('assets/js/tilt.jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/script-login.js') }}"></script>
 <script >
     $('.js-tilt').tilt({
         scale: 1.1
