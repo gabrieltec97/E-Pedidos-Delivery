4b1f0211eee8def4177d2beb7a054c9d7223d595
Gabriel Pereira <47263072+gabrieltec97@users.noreply.github.com>
2025-04-22 15:56:30 -0300

    ajuste de responsividade em tela de login

--- resources/views/auth/Login.blade.php
+++ resources/views/auth/Login.blade.php
@@ -20,63 +20,71 @@
 <div class="limiter">
     <div class="container-login100">
         <div class="wrap-login100">
-            <div class="login100-pic js-tilt" data-tilt>
-                <img src="img/img-01.png" class="img-login" alt="IMG">
-            </div>
 
-            <div class="div-login">
-                <form class="login100-form validate-form" action="{{ route('login.perform') }}" method="post">
-                    @csrf
-                        <span class="login100-form-title text-black">
+            <div class="row">
+                <div class="col-8 d-sm-none d-lg-block">
+                    <div class="login100-pic js-tilt" data-tilt>
+                        <img src="img/img-01.png" class="img-login" alt="IMG">
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-4 col-sm-12">
+                    <div class="div-login">
+                        <form class="login100-form validate-form" action="{{ route('login.perform') }}" method="post">
+                            @csrf
+                            <span class="login100-form-title text-black">
                            <b>Área de membros</b>
                         </span>
 
-                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                        <input class="input100" type="text" name="email" placeholder="Email">
-                        <span class="focus-input100"></span>
-                        <span class="symbol-input100">
+                            <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                <input class="input100" type="text" name="email" placeholder="Email">
+                                <span class="focus-input100"></span>
+                                <span class="symbol-input100">
                                 <i class="fa fa-envelope" aria-hidden="true"></i>
                             </span>
-                    </div>
+                            </div>
 
-                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                        <input class="input100" type="password" name="password" placeholder="Senha">
-                        <span class="focus-input100"></span>
-                        <span class="symbol-input100">
+                            <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                                <input class="input100" type="password" name="password" placeholder="Senha">
+                                <span class="focus-input100"></span>
+                                <span class="symbol-input100">
                                 <i class="fa fa-lock" aria-hidden="true"></i>
                             </span>
-                    </div>
+                            </div>
 
-                    <div class="container-login100-form-btn">
-                        <button class="login100-form-btn" type="submit">
-                            <span class="login-label">Login</span>
-                        </button>
-                    </div>
+                            <div class="container-login100-form-btn">
+                                <button class="login100-form-btn" type="submit">
+                                    <span class="login-label">Login</span>
+                                </button>
+                            </div>
 
-                    <div class="text-center p-t-12">
+                            <div class="text-center p-t-12">
                             <span class="txt1">
                                 Esqueceu
                             </span>
-                        <a class="txt2 pass-recover">
-                            sua senha?
-                        </a>
-                    </div>
+                                <a class="txt2 pass-recover">
+                                    sua senha?
+                                </a>
+                            </div>
 
-                    <div class="text-center p-t-136">
-                        <a class="txt2" href="#">
-                            Suporte E-Pedidos
-                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                        </a>
+                            <div class="text-center p-t-136">
+                                <a class="txt2" href="#">
+                                    Suporte E-Pedidos
+                                    <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                                </a>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
 
-            <div class="div-recover">
+            <div class="col-12 col-lg-4 col-sm-12">
+                <div class="div-recover">
                     <span class="login100-form-title text-black font-weight-bold">
                         Recuperação de senha:
                     </span>
                     <div class="wrap-input100 validate-input mt-3" data-validate = "Valid email is required: ex@abc.xyz">
-                        <input class="input100 myEmail" type="email" name="email" placeholder="Email">
+                        <input class="input100 myEmail w-100" type="email" name="email" placeholder="Email">
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                                 <i class="fa fa-envelope" aria-hidden="true"></i>
@@ -92,46 +100,47 @@
                             ao login.
                         </a>
                     </div>
-            </div>
+                </div>
 
-            <div class="div-insert">
-                <form id="recover-form" method="post">
-                    @csrf
-                    <span class="login100-form-title text-black font-weight-bold">
+                <div class="div-insert">
+                    <form id="recover-form" method="post">
+                        @csrf
+                        <span class="login100-form-title text-black font-weight-bold">
                         Insira seu código:
                     </span>
-                    <div class="wrap-input100 validate-input mt-3" data-validate = "Valid email is required: ex@abc.xyz">
-                        <input class="input100 myCode" type="number" name="code" placeholder="Código enviado ao e-mail" required>
-                        <span class="focus-input100"></span>
-                        <span class="symbol-input100">
+                        <div class="wrap-input100 validate-input mt-3" data-validate = "Valid email is required: ex@abc.xyz">
+                            <input class="input100 myCode" type="number" name="code" placeholder="Código enviado ao e-mail" required>
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
                                 <i class="fa fa-envelope" aria-hidden="true"></i>
                             </span>
-                    </div>
+                        </div>
 
-                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                        <input class="input100 pass1" type="password" name="password" placeholder="Insira a nova senha">
-                        <span class="focus-input100"></span>
-                        <span class="symbol-input100">
+                        <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                            <input class="input100 pass1" type="password" name="password" placeholder="Insira a nova senha">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
                                 <i class="fa fa-lock" aria-hidden="true"></i>
                             </span>
-                    </div>
+                        </div>
 
-                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
-                        <input class="input100 pass2" type="password" name="password2" placeholder="Confirme sua senha">
-                        <span class="focus-input100"></span>
-                        <span class="symbol-input100"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                    </div>
+                        <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                            <input class="input100 pass2" type="password" name="password2" placeholder="Confirme sua senha">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100"><i class="fa fa-lock" aria-hidden="true"></i></span>
+                        </div>
 
-                    <button class="btn btn-primary mt-2 w-100 font-weight-bold change-pass">Salvar</button>
-                </form>
+                        <button class="btn btn-primary mt-2 w-100 font-weight-bold change-pass">Salvar</button>
+                    </form>
 
-                <div class="text-center p-t-12">
+                    <div class="text-center p-t-12">
                             <span class="txt1">
                                 Voltar
                             </span>
-                    <a class="txt2 back-login">
-                        ao login.
-                    </a>
+                        <a class="txt2 back-login">
+                            ao login.
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
