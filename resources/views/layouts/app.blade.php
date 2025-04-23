08b32303e842ec2366d210ed31805922191effb5
Gabriel Pereira <gabrieldeveloper23@hotmail.com>
2025-04-23 19:10:46 -0300

    Refatoração de código

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,16 +9,12 @@
     <title>
         @yield('title')
     </title>
-    <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
     <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/jquerytoast.css') }}">
-    <!-- CSS Files -->
     <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.css') }}" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/757bf4501b.js" crossorigin="anonymous"></script>
     <script src="{{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
