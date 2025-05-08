c8ae76ba5b6619912f6a6b14e656cddc5bb4e41b
Gabriel Pereira <47263072+gabrieltec97@users.noreply.github.com>
2025-05-08 11:13:46 -0300

    Update live-orders.blade.php
    Ajuste fino 1

--- resources/views/Orders/live-orders.blade.php
+++ resources/views/Orders/live-orders.blade.php
@@ -8,7 +8,7 @@
             <div class="col-12">
                 <div class="card mb-4">
                     <div class="card-header pb-2">
-                        <h3 class="text-black">Pedidos em Tempo Real</h3>
+                        <h3 class="text-black">Pedidos em tempo real</h3>
                     </div>
                     <div class="card-body px-0 pt-0 pb-2">
                         <div class="table-responsive p-0">
